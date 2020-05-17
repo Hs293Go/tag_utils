5a5e2734e7d834cf400b49ea1b04c30baf373cee
Hs293Go <hellston20a@gmail.com>
2020-05-17 23:13:22 +0800

    operational camera node

---
+++ src/camera_node.cpp
@@ -0,0 +1,139 @@
+#include <chrono>
+#include <iostream>
+
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
+#include <camera_info_manager/camera_info_manager.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <ros/package.h>
+#include <ros/ros.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/SetCameraInfo.h>
+
+#include "opencv2/videoio.hpp"
+#include "opencv2/opencv.hpp"
+#include "unistd.h"
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "camera");
+
+    ros::NodeHandle nh("~");
+    image_transport::ImageTransport it(nh);
+
+    sensor_msgs::ImagePtr msg;
+    sensor_msgs::CameraInfo c_info;
+
+    // Parse camera name argument first
+    std::string camera_name = "camera";
+    if (nh.getParam("camera_name", camera_name))
+    {
+        ROS_INFO("Provided camera name: %s", camera_name.c_str());
+    }
+
+    // Default path to camera calibration file is guessed from camera name
+    std::string camera_info_url = "file://" + ros::package::getPath("tag_utils") + "/camera_info/" + camera_name + ".yaml";
+    if (nh.getParam("camera_info_url", camera_info_url))
+    {
+        camera_info_url = "file://" + fs::absolute(camera_info_url).generic_string();
+    }
+
+    camera_info_manager::CameraInfoManager c_info_man(nh, camera_name, camera_info_url);
+    if (!c_info_man.loadCameraInfo(camera_info_url))
+    {
+        ROS_ERROR("Calibration file not found");
+        return 1;
+    }
+    else
+    {
+        ROS_INFO("Camera successfully calibrated");
+        c_info = c_info_man.getCameraInfo();
+    }
+
+    int capture_width;
+    if (!nh.getParam("capture_width", capture_width))
+    {
+        capture_width = c_info.width;
+    }
+
+    int capture_height;
+    if (!nh.getParam("capture_height", capture_width))
+    {
+        capture_height = c_info.height;
+    }
+
+    int display_width;
+    if (!nh.getParam("display_width", display_width))
+    {
+        display_width = c_info.width;
+    }
+
+    int display_height;
+    if (!nh.getParam("display_height", display_height))
+    {
+        display_height = c_info.height;
+    }
+
+    int framerate;
+    if (!nh.getParam("framerate", framerate))
+    {
+        framerate = 30;
+    }
+
+    int flip_method;
+    if (!nh.getParam("flip_method", flip_method))
+    {
+        flip_method = 0;
+    }
+
+    std::stringstream pipe_stream;
+
+    pipe_stream << "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" << capture_width << ", height=(int)" << capture_height
+                    << ", format=(string)NV12, framerate=(fraction)" << framerate << "/1 ! nvvidconv flip-method="
+                    << flip_method << " ! video/x-raw, width=(int)" << display_width << ", height=(int)" << display_height
+                    << ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+
+    std::string pipeline = pipe_stream.str();
+
+    ROS_INFO("Using gstreamer pipeline: \x1B[32m%s", pipeline.c_str());
+
+    cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
+
+    if (!cap.isOpened())
+    {
+        ROS_ERROR("Failed to open camera.");
+        return 1;
+    }
+
+    image_transport::Publisher camera_image_publisher = it.advertise("/" + camera_name + "/image_rect", 1);
+
+    ros::Publisher camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("/" + camera_name + "/camera_info", 1);
+
+    ros::Rate loop_rate(30);
+
+    cv::Mat frame, gray;
+    while (ros::ok())
+    {
+        cap >> frame;
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+        if (!gray.empty())
+        {
+            auto current_time = ros::Time::now();
+            msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", gray).toImageMsg();
+            msg->header.stamp = current_time;
+            c_info.header.stamp = current_time;
+            camera_image_publisher.publish(msg);
+            camera_info_pub.publish(c_info);
+        }
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
