9fbc39b6176daaaef2b631f5c80e70b1125e5aad
Hs293Go <hellston20a@gmail.com>
2020-11-04 02:03:49 +0800

    Squashed - rewrite camera_node and transform_backend

--- src/camera_node.cpp
+++ src/camera_node.cpp
@@ -1,27 +1,25 @@
+#include <array>
 #include <chrono>
 #include <iostream>
 
-#include <boost/filesystem.hpp>
-#include <boost/program_options.hpp>
-
 #include <camera_info_manager/camera_info_manager.h>
 #include <cv_bridge/cv_bridge.h>
+#include <fmt/core.h>
 #include <image_transport/image_transport.h>
 #include <ros/package.h>
 #include <ros/ros.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/SetCameraInfo.h>
+#include <boost/filesystem.hpp>
 
-#include "opencv2/videoio.hpp"
 #include "opencv2/opencv.hpp"
+#include "opencv2/videoio.hpp"
 #include "unistd.h"
 
-namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     ros::init(argc, argv, "camera");
 
     ros::NodeHandle nh("~");
@@ -32,104 +30,99 @@ int main(int argc, char *argv[])
 
     // Parse camera name argument first
     std::string camera_name = "camera";
-    if (nh.getParam("camera_name", camera_name))
-    {
+    if (nh.getParam("camera_name", camera_name)) {
         ROS_INFO("Provided camera name: %s", camera_name.c_str());
     }
 
     // Default path to camera calibration file is guessed from camera name
-    std::string camera_info_url = "file://" + ros::package::getPath("tag_utils") + "/camera_info/" + camera_name + ".yaml";
-    if (nh.getParam("camera_info_url", camera_info_url))
-    {
-        camera_info_url = "file://" + fs::absolute(camera_info_url).generic_string();
+    std::string camera_info_url =
+        fmt::format("file://{}/camera_info/{}.yaml",
+                    ros::package::getPath("tag_utils"), camera_name);
+
+    if (nh.getParam("camera_info_url", camera_info_url)) {
+        camera_info_url = fmt::format(
+            "file://{}", fs::absolute(camera_info_url).generic_string());
     }
 
-    camera_info_manager::CameraInfoManager c_info_man(nh, camera_name, camera_info_url);
-    if (!c_info_man.loadCameraInfo(camera_info_url))
-    {
+    camera_info_manager::CameraInfoManager c_info_man(nh, camera_name,
+                                                      camera_info_url);
+    if (!c_info_man.loadCameraInfo(camera_info_url)) {
         ROS_ERROR("Calibration file not found");
         return 1;
-    }
-    else
-    {
+    } else {
         ROS_INFO("Camera successfully calibrated");
         c_info = c_info_man.getCameraInfo();
     }
 
     int capture_width;
-    if (!nh.getParam("capture_width", capture_width))
-    {
+    if (!nh.getParam("capture_width", capture_width)) {
         capture_width = c_info.width;
     }
 
     int capture_height;
-    if (!nh.getParam("capture_height", capture_width))
-    {
+    if (!nh.getParam("capture_height", capture_width)) {
         capture_height = c_info.height;
     }
 
     int display_width;
-    if (!nh.getParam("display_width", display_width))
-    {
+    if (!nh.getParam("display_width", display_width)) {
         display_width = c_info.width;
     }
 
     int display_height;
-    if (!nh.getParam("display_height", display_height))
-    {
+    if (!nh.getParam("display_height", display_height)) {
         display_height = c_info.height;
     }
 
     int framerate;
-    if (!nh.getParam("framerate", framerate))
-    {
+    if (!nh.getParam("framerate", framerate)) {
         framerate = 30;
     }
 
     int flip_method;
-    if (!nh.getParam("flip_method", flip_method))
-    {
+    if (!nh.getParam("flip_method", flip_method)) {
         flip_method = 0;
     }
 
-    std::stringstream pipe_stream;
-
-    pipe_stream << "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" << capture_width << ", height=(int)" << capture_height
-                    << ", format=(string)NV12, framerate=(fraction)" << framerate << "/1 ! nvvidconv flip-method="
-                    << flip_method << " ! video/x-raw, width=(int)" << display_width << ", height=(int)" << display_height
-                    << ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
-
-    std::string pipeline = pipe_stream.str();
+    const std::string pipeline = fmt::format(
+        "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int){}, "
+        "height=(int){}, format=(string)NV12, framerate=(fraction){}/1 ! "
+        "nvvidconv flip-method={} ! video/x-raw, width=(int){}, "
+        "height=(int){}, format=(string)BGRx ! videoconvert ! video/x-raw, "
+        "format=(string)BGR ! appsink",
+        capture_width, capture_height, framerate, flip_method, display_width,
+        display_height);
 
     ROS_INFO("Using gstreamer pipeline: \x1B[32m%s", pipeline.c_str());
 
     cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
 
-    if (!cap.isOpened())
-    {
+    if (!cap.isOpened()) {
         ROS_ERROR("Failed to open camera.");
         return 1;
     }
+    const auto img_pub_name = fmt::format("/{}/image_rect", camera_name);
+    auto img_pub = it.advertise(img_pub_name, 1);
+    ROS_INFO("Publishing images to: %s", img_pub_name.c_str());
 
-    image_transport::Publisher camera_image_publisher = it.advertise("/" + camera_name + "/image_rect", 1);
+    const auto info_pub_name = fmt::format("/{}/camera_info", camera_name);
+    auto info_pub = nh.advertise<sensor_msgs::CameraInfo>(info_pub_name, 1);
+    ROS_INFO("Publishing info to: %s", info_pub_name.c_str());
 
-    ros::Publisher camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("/" + camera_name + "/camera_info", 1);
-
-    ros::Rate loop_rate(30);
+    ros::Rate loop_rate(framerate);
 
     cv::Mat frame, gray;
-    while (ros::ok())
-    {
+    while (ros::ok()) {
         cap >> frame;
         cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-        if (!gray.empty())
-        {
+        if (!gray.empty()) {
             auto current_time = ros::Time::now();
-            msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", gray).toImageMsg();
+            msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", gray)
+                      .toImageMsg();
             msg->header.stamp = current_time;
             c_info.header.stamp = current_time;
-            camera_image_publisher.publish(msg);
-            camera_info_pub.publish(c_info);
+            img_pub.publish(msg);
+            info_pub.publish(c_info);
         }
         ros::spinOnce();
         loop_rate.sleep();
--- src/transform_backend.cpp
+++ src/transform_backend.cpp
@@ -1,71 +1,114 @@
-#include <ros/ros.h>
+#include <functional>
+
+#include <Eigen/Dense>
 #include <Eigen/Geometry>
 
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/Transform.h>
-#include <geometry_msgs/TwistStamped.h>
-#include <geometry_msgs/Quaternion.h>
-#include <tf2_eigen/tf2_eigen.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <tf2_ros/transform_listener.h>
+#include <apriltag_ros/AprilTagDetectionArray.h>
+#include <ros/ros.h>
+#include <sophus/se3.hpp>
 
-int main(int argc, char *argv[])
-{
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/Quaternion.h>
+#include <std_msgs/Float32MultiArray.h>
+
+int main(int argc, char *argv[]) {
     ros::init(argc, argv, "transform_backend");
 
     ros::NodeHandle nh;
 
-    ros::Subscriber sub_tf_;
-    tf2_ros::TransformBroadcaster broadcaster;
-
-    tf2_ros::Buffer tf_buffer;
-    tf2_ros::TransformListener listener(tf_buffer);
-    ros::Publisher pose_publisher = nh.advertise<geometry_msgs::PoseStamped>("/pose_tag_body", 1);
-
     std::string body_frame_name;
-    if (!nh.getParam("/body_frame", body_frame_name))
-    {
+    if (!nh.getParam("/body_frame", body_frame_name)) {
         ROS_INFO("Using default body frame name: %s", body_frame_name.c_str());
-    }
-    else
-    {
+    } else {
         ROS_INFO("Got body frame name: %s", body_frame_name.c_str());
     }
 
     // Read transformation matrix from transforms.yaml into std::vector
-    std::vector<double> vec_camera_body;
-    if (!nh.getParam("/transform_body_to_camera", vec_camera_body))
-    {
-        ROS_ERROR("No transformation matrix from camera to body provided");
+    std::vector<double> quat_tmp, pos_tmp;
+    if (!nh.getParam("/rotation", quat_tmp)) {
+        ROS_ERROR("No rotation from camera to body provided");
     }
 
-    if (vec_camera_body.size() != 16)
-    {
-        ROS_ERROR("Not a valid transformation matrix from camera to body. Has %lu elements", vec_camera_body.size());
+    if (!nh.getParam("/translation", pos_tmp)) {
+        ROS_ERROR("No translation from body to camera provided");
     }
 
-    // Assuming transformation matrix is listed in transforms.yaml in row-major order
-    // Use Eigen::Map to move data into Eigen::Matrix using Stride parameter to account for row-major order
-    Eigen::Matrix4d mat_camera_body = Eigen::Map<Eigen::Matrix4d, Eigen::Unaligned, Eigen::Stride<1, 4>>(vec_camera_body.data());
-    ROS_INFO("Got translation of camera frame relative to body frame %.3f m-x, %.3f m-y, %.3f m-z", mat_camera_body(0, 3), mat_camera_body(1, 3), mat_camera_body(2, 3));
+    if (quat_tmp.size() != 4) {
+        ROS_ERROR(
+            "Not a valid Quaternion representing rotation from camera to body. "
+            "Has %lu elements",
+            quat_tmp.size());
+    }
 
-    // Convert rotation of camera frame relative to body frame into euler angles for display
-    Eigen::Vector3d eul(mat_camera_body.topLeftCorner<3, 3>().eulerAngles(3, 2, 1));
-    ROS_INFO("Got rotation of camera frame relative to body frame %.3f deg-x, %.3f deg-y, %.3f-deg z", eul(0) * 57.296, eul(1) * 57.296, eul(2) * 57.296);
+    if (pos_tmp.size() != 3) {
+        ROS_ERROR(
+            "Not a valid vector representing translation from body to camera. "
+            "Has %lu elements",
+            pos_tmp.size());
+    }
 
-    // Convert transformation matrix into TransformStamped to work with tf2
-    geometry_msgs::TransformStamped tf_camera_body = tf2::eigenToTransform(Eigen::Isometry3d(mat_camera_body));
-    tf_camera_body.header.frame_id = body_frame_name;
-    tf_camera_body.child_frame_id = "camera";
+    const Eigen::Map<const Eigen::Quaterniond> q_c_b(quat_tmp.data());
+    const Eigen::Map<const Eigen::Vector3d> r_c_b_c(pos_tmp.data());
+
+    ROS_INFO(
+        "Got translation of camera frame relative to body frame [%.3f, %.3f, "
+        "%.3f] m",
+        -pos_tmp[0], -pos_tmp[1], -pos_tmp[2]);
+
+    // Convert rotation of camera frame relative to body frame into euler angles
+    // for display
+    constexpr double k180ByPi = 57.29577951;
+
+    const Eigen::Vector3d eul =
+        k180ByPi * q_c_b.toRotationMatrix().eulerAngles(2, 1, 0);
+
+    ROS_INFO(
+        "Got rotation of body frame relative to camera frame %.3f deg-x, %.3f "
+        "deg-y, %.3f-deg z",
+        eul[0], eul[1], eul[2]);
+
+    ros::Publisher pose_publisher =
+        nh.advertise<geometry_msgs::PoseStamped>("/pose_tag_body", 1);
+
+    ros::Subscriber pose_subscriber =
+        nh.subscribe<apriltag_ros::AprilTagDetectionArray>(
+            "/tag_detections", 100, [&](const auto &msg) {
+                using Eigen::Map;
+                using Eigen::Vector3d;
+                using Eigen::Quaterniond;
+
+                if (msg->detections.size() > 1) {
+                    ROS_WARN("Detection for %lu tags is not implemented",
+                             msg->detections.size());
+                    return;
+                }
+
+                if (msg->detections.size() == 0) {
+                    return;
+                }
+                geometry_msgs::PoseStamped tf_tag_body;
+
+                const auto &T_p_c = msg->detections[0].pose.pose.pose;
+
+                const Map<const Vector3d> r_c_p_c(&T_p_c.position.x);
+                const Map<const Quaterniond> q_p_c(&T_p_c.orientation.x);
+
+                Map<Vector3d> r_b_p_b(&tf_tag_body.pose.position.x);
+                Map<Quaterniond> q_p_b(&tf_tag_body.pose.orientation.x);
+
+                q_p_b = q_p_c * q_c_b;
+                r_b_p_b = q_c_b.inverse() * (r_c_p_c - r_c_b_c);
+
+                tf_tag_body.header = msg->detections[0].pose.header;
+
+                pose_publisher.publish(tf_tag_body);
+            });
 
     ros::Rate loop_rate(30);
 
-    while (ros::ok())
-    {
-        tf_camera_body.header.stamp = ros::Time::now();
-        broadcaster.sendTransform(tf_camera_body);
-
+    while (ros::ok()) {
         ros::spinOnce();
         loop_rate.sleep();
     }
